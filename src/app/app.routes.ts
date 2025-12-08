f7819387f7191bf0bb9b70ea630f909f0ead06b5
Adrien Le Corre <adrilc3329@hotmail.fr>
2025-12-08 10:27:55 +0100

    Page Catalogue / Navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CatalogueComponent } from './components/catalogue/catalogue';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'catalogue', component: CatalogueComponent }
+];
---
+++ src/app/components/catalogue/catalogue.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CatalogueComponent } from './catalogue';
+
+describe('CatalogueComponent', () => {
+  let component: CatalogueComponent;
+  let fixture: ComponentFixture<CatalogueComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CatalogueComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CatalogueComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
