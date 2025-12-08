c34601f2ce15205be7d880d4801ed38115cd0655
RIUNE MARIE <riunemarie@gmail.com>
2025-12-08 15:05:34 +0100

    ajout panier

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CatalogueComponent } from './components/catalogue/catalogue';
+import { PanierComponent } from './components/panier/panier';
 
 export const routes: Routes = [
-    { path: 'catalogue', component: CatalogueComponent }
+    { path: 'catalogue', component: CatalogueComponent },
+    { path: 'panier', component: PanierComponent }
 ];
---
+++ src/app/components/panier/panier.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PanierService } from '../../services/panier.service'; 
+import { Vinyle } from '../catalogue/catalogue'; 
+
+interface PanierItem {
+  vinyle: Vinyle;
+  qte: number;
+}
+
+@Component({
+  selector: 'app-panier',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './panier.html',
+})
+export class PanierComponent implements OnInit {
+  
+  items: PanierItem[] = [];
+
+  constructor(private panierService: PanierService) {}
+
+  ngOnInit() {
+    this.panierService.panier$.subscribe(items => {
+      this.items = items;
+    });
+  }
+
+  incrementer(item: PanierItem) {
+    this.panierService.changerQuantite(item.vinyle, item.qte + 1);
+  }
+
+  decrementer(item: PanierItem) {
+    if (item.qte > 1) {
+      this.panierService.changerQuantite(item.vinyle, item.qte - 1);
+    } else {
+      this.supprimer(item);
+    }
+  }
+
+  supprimer(item: PanierItem) {
+    this.panierService.retirer(item.vinyle);
+  }
+
+  vider() {
+    this.panierService.viderPanier();
+  }
+
+  total() {
+    return this.items.reduce(
+      (sum, item) => sum + item.vinyle.prix_vinyle * item.qte,
+      0
+    );
+  }
+}
