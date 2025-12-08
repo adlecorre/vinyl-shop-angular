c92cc6f157c43258f09e0ffdc54bfaedc8407b34
Élodie Chau <elodie.chau@labom2iformation.fr>
2025-12-08 13:57:25 +0100

    Ajout inscription/connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ConnexionComponent } from './components/connexion/connexion';
+import { InscriptionComponent } from './components/inscription/inscription';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'connexion', component: ConnexionComponent},
+    {path: 'inscription', component: InscriptionComponent}
+];
